172f197a7e908e84d0f62fd42db72d2b77264247
alex39998 <105219087+alex39998@users.noreply.github.com>
2022-05-10 08:26:14 -0500

    Update Line Integral Visualization.py
    3D Visualization of a line integral

--- Line Integral Visualization.py
+++ Line Integral Visualization.py
@@ -1,4 +1,3 @@
-
 import matplotlib.patches as mpatches
 import matplotlib.animation as mtA
 import numpy as np
@@ -15,6 +14,23 @@ y=np.sin(t)
 rectangles=30
 interval=(t_final-t_init)/rectangles
 
+def fill_between_3d(ax, x1, y1, z1, x2, y2, z2, mode=1, c='steelblue', alpha=0.6):
+    if mode == 1:
+
+        for i in range(len(x1) - 1):
+            verts = [(x1[i], y1[i], z1[i]), (x1[i + 1], y1[i + 1], z1[i + 1])] + \
+                    [(x2[i + 1], y2[i + 1], z2[i + 1]), (x2[i], y2[i], z2[i])]
+
+            ax.add_collection3d(Poly3DCollection([verts],
+                                                 alpha=alpha,
+                                                 linewidths=0,
+                                                 color=c))
+
+    if mode == 2:
+        verts = [(x1[i], y1[i], z1[i]) for i in range(len(x1))] + \
+                [(x2[i], y2[i], z2[i]) for i in range(len(x2))]
+
+        ax.add_collection3d(Poly3DCollection([verts], alpha=alpha, color=c))
 
 #parameterization of curve
 def x_param ():
@@ -30,28 +46,47 @@ def z_param ():
     return z
 
 def reinman ():
-    #Loop through  from start point to end point
-    #Interval = (end-start)/rectangles
-    #Given a parametric x=x(t) and y=y(t)
-    #x_n = x_n(start + interval*n)
-    #y_n=y_n(start + interval*n)
-    #Every rectangle:
-    #plt.fill_between_3d()
+   #interval = (t_final - t_init) / rectangles
 
-    
-    #for h in range(0,interval):
-
-        #return
+    """for i in range(1, rectangles + 1):
+        val[i] = np.sin(t_init + interval * i)
+        ts[i] = t_init + interval * i
+        tp[i] = t_init + interval * (i - 1)"""
     pass
 
 def main ():
     fig =plt.figure()
     ax=fig.add_subplot(projection='3d')
     ax.plot(x_param(),y_param(),z_param())
-
     
-
+    set01xtoline = [x_param(), y_param(), np.zeros(len(t))]
+    set1 = [x_param(), y_param(), z_param()]
+    fill_between_3d(ax, *set01xtoline, *set1, mode=1, c="C0")
     plt.show()
 
 if __name__ == '__main__':
     main()
+
+  """
+    Function similar to the matplotlib.pyplot.fill_between function but 
+    for 3D plots.
+    input:
+        ax -> The axis where the function will plot.
+
+        x1 -> 1D array. x coordinates of the first line.
+        y1 -> 1D array. y coordinates of the first line.
+        z1 -> 1D array. z coordinates of the first line.
+
+        x2 -> 1D array. x coordinates of the second line.
+        y2 -> 1D array. y coordinates of the second line.
+        z2 -> 1D array. z coordinates of the second line.
+    modes:
+        mode = 1 -> Fill between the lines using the shortest distance between 
+                    both. Makes a lot of single trapezoids in the diagonals 
+                    between lines and then adds them into a single collection.
+        mode = 2 -> Uses the lines as the edges of one only 3d polygon.
+
+    Other parameters (for matplotlib): 
+        c -> the color of the polygon collection.
+        alpha -> transparency of the polygon collection.
+    """
